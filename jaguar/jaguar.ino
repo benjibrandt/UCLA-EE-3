b37d60a648425ab040b55fd73785a2c0843c0907
Benji Brandt <benjibrandt@ucla.edu>
2017-11-09 11:33:36 -0800

    Added transistor constants and test cases.

--- jaguar/jaguar.ino
+++ jaguar/jaguar.ino
@@ -1,16 +1,24 @@
 ///////////////////////////
 // CONSTANTS
 //////////////////////////
-#define GREEN_LED 12
-#define BLUE_LED 11
-#define RED_LED 10
-#define LHS_MOTOR 3 // TIP 120
-#define RHS_MOTOR 5 // TIP 125
+//-----  NAME -------------- PIN LABEL -----
+#define GREEN_LED 12        // D12
+#define BLUE_LED 11         // D11
+#define RED_LED 10          // D10
+#define LHS_MOTOR 3         // D3; TIP 120 (responds to high)
+#define RHS_MOTOR 5         // D5; TIP 125 (responds to low)
+#define RHS_TRANSISTOR 14   // A0
+#define LHS_TRANSISTOR 19   // A5
+#define MID_TRANSISTOR 17   // A3
 
 ///////////////////////////
 // FUNCTION DELCARATIONS
 //////////////////////////
-void initLEDS();
+
+// Sets pinMode for all visible light LEDs.
+void initVisibleLEDS();
+
+// Sets pinMode for the two motors.
 void initMotors();
 
 ///////////////////////////
@@ -18,7 +26,7 @@ void initMotors();
 //////////////////////////
 void setup() 
 {
-  initLEDS();
+  initVisibleLEDS();
   initMotors();
   Serial.begin(9600);
 
@@ -26,18 +34,47 @@ void setup()
 
 void loop()
 {
-  digitalWrite(GREEN_LED, HIGH);
-  digitalWrite(BLUE_LED, HIGH);
-  digitalWrite(RED_LED, HIGH);
+  // Test RHS
+  if( digitalRead(RHS_TRANSISTOR) == HIGH )
+  {
+    digitalWrite(GREEN_LED, HIGH);
+  }
+  else
+  {
+    digitalWrite(GREEN_LED, HIGH);
+    digitalWrite(RED_LED, HIGH);
+  }
+  // Test LHS
+  if( digitalRead(LHS_TRANSISTOR) == HIGH )
+  {
+    digitalWrite(BLUE_LED, HIGH);
+  }
+  else
+  {
+    digitalWrite(BLUE_LED, HIGH);
+    digitalWrite(RED_LED, HIGH);
+  }
+  // Test MID
+  if( digitalRead(MID_TRANSISTOR) == HIGH )
+  {
+    digitalWrite(RED_LED, HIGH);
+  }
+  else
+  {
+    digitalWrite(GREEN_LED, HIGH);
+    digitalWrite(BLUE_LED, HIGH);
+    digitalWrite(RED_LED, HIGH);
+  }
+  
 
-  analogWrite(LHS_MOTOR, HIGH);
-  analogWrite(RHS_MOTOR, LOW);
+  // analogWrite(LHS_MOTOR, HIGH);
+  // analogWrite(RHS_MOTOR, LOW);
 }
 
 ///////////////////////////
 // FUNCTION DEFINITIONS
 //////////////////////////
-void initLEDS()
+void initVisibleLEDS()
 {
   pinMode(GREEN_LED, OUTPUT);
   pinMode(BLUE_LED, OUTPUT);
